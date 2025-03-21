a8c09e4cb73e6994fe68d66bb4094fd174576888
Captainistz <captainistz@gmail.com>
2025-03-22 04:45:38 +0700

    ✨ Refactor layout and enhance site header and footer components

---
+++ src/components/site-footer.tsx
@@ -0,0 +1,15 @@
+const SiteFooter = () => {
+  return (
+    <footer className='border-grid border-t py-6 md:py-0'>
+      <div className='container-wrapper'>
+        <div className='container py-4 items-center'>
+          <div className='text-center text-sm leading-loose text-muted-foreground'>
+            Made with 🤍 | Team Blackened
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default SiteFooter
